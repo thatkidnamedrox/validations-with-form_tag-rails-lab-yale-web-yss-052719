be6a96c1f79ab31b3d3cd6bd80280e1277cea406
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-08 03:48:41 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
