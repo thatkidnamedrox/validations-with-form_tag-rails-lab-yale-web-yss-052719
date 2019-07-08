98d41d9243e3f47f09e2121b672d943c54e75d5d
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-08 03:39:55 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validate :category_options
+
+  def category_options
+    options = ["Fiction", "Non-Fiction"]
+    if !options.include?(category)
+      errors.add(:category, "must be either fiction or non-fiction")
+    end
+  end
+  
 end
